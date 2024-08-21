245a47d29307c5c18f8a541438c4cebd0e2c75f4
Alexander <sany199574@gmail.com>
2024-08-21 15:23:47 +0300

    Добавлен проект

---
+++ src/PS.Strawberry_Drinks_Store/PS.Strawberry_Drinks_Store.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PS.Strawberry_Drinks_Store.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
